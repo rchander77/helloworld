6219fa6910d33009862f94f63a0851ac730e651c
rchander77 <146034097+rchander77@users.noreply.github.com>
2024-01-24 23:47:53 -0600

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,52 @@
+INSERT INTO Organization
+(name, address, city, state, manager, phone, email)
+VALUES('Junior Achievement of the Heartland', '800 12th Avenue', 'Moline', 'IL', 'John', '(309) 762-1630', 'jaoh@gmailcom');
+INSERT INTO Organization
+(name, address, city, state, manager, phone, email)
+VALUES('GroWorks', '1000 21st St', 'Rock Island', 'IL', 'Mathew', '(309) 786-6944', 'groworks@gmailcom');
+INSERT INTO Organization
+(name, address, city, state, manager, phone, email)
+VALUES('Quad Citians Affirming Diversity', '1705 2nd Ave, Suite 508', 'Rock Island', 'IL', 'Mathew', '(309) 786-2580', 'qcad@gmailcom');
+INSERT INTO Organization
+(name, address, city, state, manager, phone, email)
+VALUES('Family Museum', '2900 Learning Campus Drive', 'Bettendorf', 'IA', 'Samuel', '(563) 344-4106', 'fm@gmailcom');
+INSERT INTO Organization
+(name, address, city, state, manager, phone, email)
+VALUES('United Neighbors Inc. of Davenport', '808 Harriso Street', 'Davenport', 'IA', 'Dixci', '(319) 322-7363', 'uiod@gmailcom');
+INSERT INTO Organization
+(name, address, city, state, manager, phone, email)
+VALUES('AIDS Prevention Partnership', '1202 West Third Street P.O. Box 3733', 'Davenport', 'IA', 'Marry', '(319) 336-4517', 'marryd@gmailcom');
+INSERT INTO Organization
+(name, address, city, state, manager, phone, email)
+VALUES('Mississippi Valley Regional Blood Center', '5500 Lakeview Drive', 'Davenport', 'IA', 'Chris', '(800) 747-5401', 'chris@gmailcom');
+
+INSERT INTO Restaurant
+(name, address, city, state, manager, food, phone, email, status)
+VALUES('Buffalo Wild Wings', '4408 16th St', 'Moline', 'IL', 'Sam', 'burgers', '(309) 757-0774', 'order@dominos.com', 'available');
+INSERT INTO Restaurant
+(name, address, city, state, manager, food, phone, email, status)
+VALUES('Dairy Queen Grill & Chill', '2263 7th St', 'Silvis', 'IL', 'Danny', 'fast-food', '(309) 796-4641', 'order@dairyqueen.com', 'available');
+INSERT INTO Restaurant
+(name, address, city, state, manager, food, phone, email, status)
+VALUES('Deerfield Family Restaurant', '2215 John Deere Rd', 'East Moline', 'IL', 'Samuel', 'Sandwich', '(309) 792-3900', 'order@deerfieldfamily.com', 'available');
+INSERT INTO Restaurant
+(name, address, city, state, manager, food, phone, email, status)
+VALUES('Rudy''s Tacos', '1108 Avenue of the Cities', 'East Moline', 'IL', 'Austin Mount', 'nachos and burritos', '(309) 281-1422', 'order@rudystacos.com', 'available');
+INSERT INTO Restaurant
+(name, address, city, state, manager, food, phone, email, status)
+VALUES('Domino''s Pizza', '435 Avenue of the Cities', 'East Moline', 'IL', 'Marry', 'pizza', '(309) 792-0030', 'order@dominos.com', 'available');
+INSERT INTO Restaurant
+(name, address, city, state, manager, food, phone, email, status)
+VALUES('Burger King', '1222 42nd Ave', 'East Moline', 'IL', 'Austin Mount', 'burgers and French Fries', '(309) 281-1422', 'order@bk.com', 'available');
+INSERT INTO Restaurant
+(name, address, city, state, manager, food, phone, email, status)
+VALUES('KFC', '1170 42nd Ave', 'East Moline', 'IL', 'Austin Mount', 'chicken nuggets', '(309) 281-1422', 'order@kfc.com', 'available');
+INSERT INTO Restaurant
+(name, address, city, state, manager, food, phone, email, status)
+VALUES('Indian Cafe', '669 Avenue of the Cities', 'East Moline', 'IL', 'Austin Mount', 'butter chicken', '(309) 912-7604', 'order@ic.com', 'available');
+INSERT INTO Restaurant
+(name, address, city, state, manager, food, phone, email, status)
+VALUES('Subway', '681 Avenue of the Cities', 'East Moline', 'IL', 'Richard', 'sandwiches and breads', '(309) 912-7604', 'order@subway.com', 'available');
+INSERT INTO Restaurant
+(name, address, city, state, manager, food, phone, email, status)
+VALUES('888 Bistro', '633 Avenue of the Cities', 'East Moline', 'IL', 'Richard', 'Soda and Salad', '(309) 203-1986', 'order@888.com', 'available');
